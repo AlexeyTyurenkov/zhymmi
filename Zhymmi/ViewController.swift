77f7fb2790884633b82e71444957fab7036e182d
Oleksii Tiurenkov <alterego4@gmail.com>
2018-10-04 20:56:09 +0300

    Initial Commit

---
+++ Zhymmi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Zhymmi
+//
+//  Created by Aleksey Tyurenkov on 10/4/18.
+//  Copyright © 2018 com.ovt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
